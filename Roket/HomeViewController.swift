335fb7e8a29e8c4c030a4fdc311aed40d3415c48
Rui Ong <rui.ong91@gmail.com>
2017-03-17 15:39:13 +0800

    initial commit

---
+++ Roket/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roket
+//
+//  Created by Rui Ong on 17/03/2017.
+//  Copyright © 2017 Rui Ong. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
