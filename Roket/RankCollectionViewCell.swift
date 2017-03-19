784d730f14dc816f36a2903fd54c6a502ad173ce
Rui Ong <rui.ong91@gmail.com>
2017-03-19 23:29:05 +0800

    left ranking page2

---
+++ Roket/RankCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RankCollectionViewCell.swift
+//  Roket
+//
+//  Created by Rui Ong on 19/03/2017.
+//  Copyright © 2017 Rui Ong. All rights reserved.
+//
+
+import UIKit
+
+class RankCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
